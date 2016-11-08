77dfeffa497a50597080b7ac5af4da56cb2fd04f
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-08 11:15:45 +0100

    shutdown hook

--- src/main/java/pl/ppkwu/zad/Application.java
+++ src/main/java/pl/ppkwu/zad/Application.java
@@ -1,8 +1,38 @@
 package pl.ppkwu.zad;
 
+import pl.ppkwu.zad.api.FileLibrary;
+import pl.ppkwu.zad.api.RetryCallback;
+import pl.ppkwu.zad.impl.FileLibraryImpl;
+import pl.ppkwu.zad.impl.IOUtils;
+import pl.ppkwu.zad.impl.RetryCallbackImpl;
+
 public class Application {
 
-    public static void main(String[] args) {
+    private static RetryCallback retryCallback;
+    private static FileLibrary fileLibrary;
 
+    public static void main(String[] args) throws Exception {
+        retryCallback = new RetryCallbackImpl();
+        fileLibrary = new FileLibraryImpl();
+        fileLibrary.setRetryCallback(retryCallback);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println("Shutdown hook:");
+                performFileOperations();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }));
+
+        performFileOperations();
+    }
+
+    private static void performFileOperations() throws Exception {
+        String correctString = IOUtils.getInput("Poprawny string");
+        String wrongString = IOUtils.getInput("Zly string");
+        String fileName = IOUtils.getInput("Nazwa pliku");
+
+        fileLibrary.writeToFile(fileName, correctString, wrongString);
     }
 }
