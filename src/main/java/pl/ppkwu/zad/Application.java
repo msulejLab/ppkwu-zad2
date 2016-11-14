0113921b699023b25ce058587463b3b7491bbb51
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-14 20:47:48 +0100

    drobne poprawki w kliencie

--- src/main/java/pl/ppkwu/zad/Application.java
+++ src/main/java/pl/ppkwu/zad/Application.java
@@ -1,38 +1,57 @@
 package pl.ppkwu.zad;
 
+import pl.ppkwu.zad.api.ExitCallback;
 import pl.ppkwu.zad.api.FileLibrary;
 import pl.ppkwu.zad.api.RetryCallback;
+import pl.ppkwu.zad.impl.ExitCallbackImpl;
 import pl.ppkwu.zad.impl.FileLibraryImpl;
 import pl.ppkwu.zad.impl.IOUtils;
 import pl.ppkwu.zad.impl.RetryCallbackImpl;
 
 public class Application {
 
-    private static RetryCallback retryCallback;
     private static FileLibrary fileLibrary;
+    private static RetryCallback retryCallback;
+    private static ExitCallback exitCallback;
 
-    public static void main(String[] args) throws Exception {
-        retryCallback = new RetryCallbackImpl();
+    public static void main(String[] args) {
         fileLibrary = new FileLibraryImpl();
+        retryCallback = new RetryCallbackImpl();
+        exitCallback = new ExitCallbackImpl();
         fileLibrary.setRetryCallback(retryCallback);
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                System.out.println("Shutdown hook:");
-                performFileOperations();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }));
+        initializeShutdownHook();
 
         performFileOperations();
     }
 
-    private static void performFileOperations() throws Exception {
-        String correctString = IOUtils.getInput("Poprawny string");
-        String wrongString = IOUtils.getInput("Zly string");
-        String fileName = IOUtils.getInput("Nazwa pliku");
+    private static void initializeShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println("\nWykryto zdarzenie zamkniecia aplikacji");
+                Boolean confirmExit = exitCallback.call();
+                if (!confirmExit) {
+                    System.out.println("Aplikacja zostanie kontynuowana");
+                    performFileOperations();
+                } else {
+                    System.out.println("Aplikacja zostala zakonczona");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }));
+    }
 
-        fileLibrary.writeToFile(fileName, correctString, wrongString);
+    private static void performFileOperations() {
+        String correctMessage = IOUtils.getInput("Poprawny string");
+        String wrongMessage = IOUtils.getInput("Niepoprawny string");
+        String fileName = IOUtils.getInput("Nazwa pliku: ");
+
+        fileLibrary.writeToFile(fileName, correctMessage, wrongMessage);
+
+        boolean performAgain = retryCallback.call();
+        if (performAgain) {
+            performFileOperations();
+        }
     }
 }
